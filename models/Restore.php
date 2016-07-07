b7f26c8247171a7acc895a55ff96c3d4886a3457
Osman <osman.ramazanov@gmail.com>
2016-07-07 17:38:01 +0300

    init

---
+++ models/Restore.php
@@ -0,0 +1,56 @@
+<?php
+    namespace app\models;
+    use app\components\Email;
+    use Yii;
+    use yii\db\ActiveRecord;
+
+    class Restore extends ActiveRecord{
+
+        public $emailMessage;
+        private static $newPassword;
+
+        public  function afterDelete(){
+            $restoreData = self::getAttributes();
+            $user = User::findOne($restoreData['userId']);
+            $newPassword = self::$newPassword;
+            $loginLink = Yii::$app->urlManager->createAbsoluteUrl(['site/login']);
+            $title = "ВОССТАНОВЛЕНИЕ ПАРОЛЯ";
+            $body = "Уважаемый, $user->name <br>";
+            $body .= "Ваш новый пароль: $newPassword <br>";
+            $body .= "Чтобы войти пройдите по ссылке и воспользуйтесь Вашим email'ом и новым паролем <br>";
+
+            Email::sendEmail(
+                'mail-template-html',
+                Yii::$app->name . '. Восстановление пароля.',
+                $title,
+                $loginLink,
+                $body,
+                [Yii::$app->params['adminEmail'] => Yii::$app->name . ' robot'],
+                $user->email
+            );
+
+            return true;
+        }
+
+        public static function restorePassword($token){
+            $now = time();
+            $restoreData = self::findOne(['link' => $token]);
+
+            if(!is_null($restoreData) and ($restoreData->sendDate+3600*24) >= $now){
+                $user = User::findOne(['id' => $restoreData->userId]);
+
+                self::$newPassword = Yii::$app->getSecurity()
+                                           ->generateRandomString(8);
+                $restoreData->delete();
+                $user->password = Yii::$app->getSecurity()
+                                           ->generatePasswordHash(self::$newPassword);
+                $user->save(false);
+
+                return true;
+
+
+            }
+            return false;
+        }
+
+    }
---
+++ views/partner/constructor/steps/step-6.php
@@ -0,0 +1,59 @@
+<?php
+
+    use yii\helpers\Html;
+
+    /* @var $locationForm app\models\forms\LocationForm */
+
+    $user = \app\models\User::findOne(Yii::$app->user->id);
+?>
+<div class="row">
+    <a href="#" class="row-title">Укажите место проведения</a>
+    <div class="row-content">
+        <?php
+            $locations_count = count($locationForm->address);
+            if($locations_count == 0){
+                $locations_count = 1;
+            } ?>
+        <div id="locations" data-locations-count="<?= $locations_count + 1 ?>">
+            <?php
+            for($i = 0; $i < $locations_count; $i++): ?>
+                    <div id="location_1" class="address-row">
+                        <div class="required-field">
+                            <?= Html::textInput('LocationForm[address][]', $locationForm->address[$i], [
+                                'class'       => 'w-100 address',
+                                'placeholder' => 'ул. Парашютная 12/14 1',
+                                'id'          => 'address_1'
+                            ]) ?>
+                            <div class="form-error-msg f-14"></div>
+                        </div>
+                        <div class="f-0 mtop-20">
+                            <div class="dib w-50 f-14">
+                                <?= Html::textInput('LocationForm[city][]', $locationForm->city[$i], [
+                                    'class'       => 'w-85 city',
+                                    'placeholder' => 'Киев',
+                                    'disabled' => true,
+                                    'id' => 'city_address_1'
+                                ]) ?>
+                            </div>
+                            <div class="dib w-50 f-14 text-right">
+                                <?= Html::textInput('LocationForm[phone][]', $locationForm->phone[$i], [
+                                    'class'       => 'w-100 phone',
+                                    'placeholder' => '+380 (ХХ) ХХХ-ХХ-ХХ',
+                                    'pattern'     => '/^(\+?38\s?|)(|\()[0-9]{3}(|\))\s?(|\-)[0-9]{3}\s?(|\-)[0-9]{2}\s?(|\-)[0-9]{2}$/'
+                                ]) ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="db text-center mtop-50">
+                        <button type="button" id="addlocation" class="btn btn-white-blue-border btn-add">Добавить локацию</button>
+                    </div>
+            <?php endfor; ?>
+            </div>
+        <div class="db mtop-40 text-right">
+            <button class="btn btn-yellow btn-next-step">Дальше</button>
+        </div>
+    </div>
+
+
+</div>
