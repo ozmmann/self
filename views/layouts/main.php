6d0c1451417cc644eb7776e3485fb7d915ff4ace
Osman <osman.ramazanov@gmail.com>
2016-08-18 14:59:25 +0300

    Added Google Tag Manager

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -26,6 +26,15 @@ AppAsset::register($this);
 
 </head>
 <body class="<?= Yii::$app->controller->id == 'site' ? 'site ' . Yii::$app->controller->action->id : '' ?>">
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-K2X8CJ"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-K2X8CJ');</script>
+<!-- End Google Tag Manager -->
 <?php $this->beginBody() ?>
 
 <?php include_once("header.php"); ?>
