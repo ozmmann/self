672026c3f50c64feb79f7b70f0ee27a6eb96a732
Osman <osman.ramazanov@gmail.com>
2016-08-18 15:00:36 +0300

    Changed success registration text and style

--- views/site/successReg.php
+++ views/site/successReg.php
@@ -5,7 +5,7 @@ $this->title = 'Успешная регистрация';
     <div class="container">
         <div id="info_page_wrapper" class="w-85 m-auto text-center">
             <img src="/img/success-image.png" class="img-response mtop-40 mtop-lg-80">
-            <div class="f-34 lh-1-4 fw-semi-bold mtop-40">Спасибо за регистрацию!<br>
+            <div class="f-34 lh-1-4 fw-semi-bold mtop-40">Спасибо!<br>
                 Мы отправили письмо для подтверждения регистрации.
             </div>
             <div class="f-14 c-gray lh-1-4 fw-light mtop-40">
